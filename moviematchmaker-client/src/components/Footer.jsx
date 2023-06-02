0160e82394bb1997203f982774c6a524d27e9a9c
Tonio9802984 <antoine.prevors@gmail.com>
2023-06-02 17:39:28 +0200

    test for deployment

---
+++ moviematchmaker-client/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div>moviematchmaker @Tonio-meso</div>;
+};
+
+export default Footer;
--- moviematchmaker-client/src/pages/SwipePage.jsx
+++ moviematchmaker-client/src/pages/SwipePage.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+// import { useKeenSlider } from "react-keen-slider";
+// import "keen-slider/keen-slider.min.css";
 
 const MovieSlider = () => {
   console.log("here my swipe page");
+  // const [movies, setMovies] = useState(0)
+  // const [currentSlide,]
 
   return <div>hello</div>;
 };
