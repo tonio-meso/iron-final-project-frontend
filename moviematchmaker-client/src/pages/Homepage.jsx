e059c87387b0bd6aeb2959489a9448ea8736d04b
Tonio9802984 <antoine.prevors@gmail.com>
2023-06-03 19:29:21 +0200

    modif for cors

--- moviematchmaker-client/src/pages/Homepage.jsx
+++ moviematchmaker-client/src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-// import service from "./../service/api";
+import service from "./../service/api";
 
 const Homepage = () => {
   const [movies, setMovies] = useState([]);
@@ -10,8 +10,12 @@ const Homepage = () => {
     const fetchDataForTheCube = async () => {
       try {
         ///fix the url
-        const response = await axios.get("http://localhost:3000/api/allmovies");
+        //const response = await axios.get("http://localhost:3000/api/allmovies");
+        const response = await service.get(
+          "https://movie-match-maker-service.onrender.com/api/allmovies"
+        );
         setMovies(response.data);
+        console.log(response.data);
       } catch (error) {
         console.log(error);
       }
