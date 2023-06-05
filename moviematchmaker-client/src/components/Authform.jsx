04c8807ff495ca3a73c8c6a254a000030e0f54ab
Tonio9802984 <antoine.prevors@gmail.com>
2023-06-05 09:28:44 +0200

    fix for the authcontext

--- moviematchmaker-client/src/components/Authform.jsx
+++ moviematchmaker-client/src/components/Authform.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "./../context/authContext";
+import { AuthContext } from "../context/AuthContext";
 
 const AuthForm = ({ mode }) => {
   const [email, setEmail] = useState("");
